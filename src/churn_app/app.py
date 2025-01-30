a20f0fa5ac71b1b9cae9134860e82d6ab4a22246
jdaguilar <jorgeaguilar2761@gmail.com>
2025-01-29 23:11:13 -0500

    fix: transform form to dict

--- src/churn_app/app.py
+++ src/churn_app/app.py
@@ -2,24 +2,28 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
-from preprocessing import preprocess_input
+from extras.preprocessing import preprocess_input
+
 
 app = Flask(__name__)
 
 # Load the trained model
-with open('model/churn_model.pkl', 'rb') as model_file:
+with open('src/churn_app/models/log_reg_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
+    data = request.form.to_dict()
     preprocessed_data = preprocess_input(data)
     prediction = model.predict(preprocessed_data)
     return jsonify({'prediction': int(prediction[0])})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
